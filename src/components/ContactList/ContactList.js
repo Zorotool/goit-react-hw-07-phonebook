e2c89b15d7079ec995d756da4967dd99967756e6
ANDRII HALYTSKYI <galickyav@gmail.com>
2022-09-29 17:05:19 +0300

    013

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,15 +1,40 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import { useFetchContactsQuery } from '../../redux/slice';
+import { useSelector } from 'react-redux';
 import ContactItem from "components/ContactItem";
 
 
-const ContactList = ({ filtredContacts,onDeleteContact }) => (
-    <ul>
-        {filtredContacts.map(contact => (
-            <ContactItem key={contact.idKey} contact={contact} onDeleteContact={onDeleteContact}/>    
-        ))}
-        </ul>
-);
+
+function ContactList() {
+    const { data: contacts, isFetching } = useFetchContactsQuery();
+    const filter = useSelector(state => state.filter.value)
+
+    const getFiltredContacts = () => { 
+    if (!contacts) {
+      return;
+    }
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+  const filtredContacts = getFiltredContacts();
+
+return (
+    <>
+        {isFetching && <h3>Loading...</h3>}
+        {contacts && (
+            <ul>
+                {filtredContacts.map(contact => (
+                    <li key={contact.id}>
+                        <ContactItem contact={contact} />
+                    </li>
+                ))}
+           </ul> 
+        )}        
+        </>        
+    )    
+}    
     
 ContactList.propTypes = {
     contacts: PropTypes.arrayOf(PropTypes.shape),
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,63 +1,19 @@
 
 import { configureStore } from "@reduxjs/toolkit";
-import { createSlice } from "@reduxjs/toolkit";
-import {
-  persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-
-const contactsSlice =createSlice({
-  name: 'contacts',
-  initialState: {
-    items: [],
-    filter: '',
-  },
-  reducers: {
-    addContact(state, action) {
-      state.items.push(action.payload);
-    },
-    deleteContact(state, action) {
-      state.items = state.items.filter(item => item.name !== action.payload);
-    },
-    changeFilter(state, action) {
-      state.filter = action.payload;
-    },
-  }
-});
-
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['items'],
-};
-
-const contactsReducer = persistReducer(
-  persistConfig,
-  contactsSlice.reducer
-);
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { contactsApi } from './slice';
+import { filterSlice } from './filterSlice'
 
 
-
-export const { addContact, deleteContact, changeFilter } = contactsSlice.actions;
 
 
 export const store = configureStore({
-    reducer: {
-      contacts: contactsReducer,
+  reducer: {
+    filter: filterSlice.reducer,
+    [contactsApi.reducerPath]: contactsApi.reducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+ middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(contactsApi.middleware),
 });
 
-export const persistor = persistStore(store);
+setupListeners(store.dispatch);
